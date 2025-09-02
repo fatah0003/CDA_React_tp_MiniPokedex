77e0b7f5c6ee97b72a2564a21e8ef0759065f92c
fatah0003 <fatahutd@gmail.com>
2025-09-02 09:49:37 +0200

    version 1 : affichage des pokémons, et recherche d'un pokémon par nom, sauvgarde de la recherche dans le LocalStorage

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+    //data
+    const pokemons = [
+        { name: 'Pikachu', type: 'Électrik' },
+        { name: 'Bulbizarre', type: 'Plante' },
+        { name: 'Salamèche', type: 'Feu' },
+        { name: 'Carapuce', type: 'Eau' },
+    ];
+
+    // state
+    const [searchTerm, setSearchTerm] = useState(() => localStorage.getItem('Pikachu') ?? '');
+
+    useEffect(() => {
+        localStorage.setItem('Pikachu', searchTerm);
+    }, [searchTerm]);
+
+    //event
+    const handleSearch = (e) => setSearchTerm(e.target.value);
+
+    //Search
+    const filteredPokemons = pokemons.filter((pokemon) => {
+        const byName = pokemon.name.toLowerCase().includes(searchTerm.toLowerCase());
+        return byName;
+    });
+
+    return (
+        <>
+            <h1>Minipokédex</h1>
+            <InputWithLabel
+                id="search"
+                label="Search: "
+                value={searchTerm}
+                type="text"
+                onInputChange={handleSearch}
+                placeholder="Search"
+            />
+            <hr />
+            {filteredPokemons.length > 0 ? (
+  <List list={filteredPokemons} />
+) : (
+  <p>No results</p>
+)}
+        </>
+    );
+}
+
+const InputWithLabel = ({ id, label, value, type, onInputChange, placeholder }) => (
+    <>
+        <label htmlFor={id}>{label}</label>
+        <input
+            id={id}
+            type={type}
+            onChange={onInputChange}
+            value={value}
+            placeholder={placeholder}
+        />
+    </>
+);
+
+const List = ({ list }) => {
+    return (
+        <ul>
+            {list.map((item) => (
+                <li key={item.name}>
+                    {item.name}  ({item.type})
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default App;
